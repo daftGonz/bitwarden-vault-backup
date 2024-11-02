fd80063947b5a671a3dc43580a94fdcb9a0a0c11
Alex R. González <50020229+daftGonz@users.noreply.github.com>
2024-11-01 23:30:55 -0400

    Update az-kv-retrieval.sh
    Changed az command values to placeholders.

--- az-kv-retrieval.sh
+++ az-kv-retrieval.sh
@@ -29,15 +29,15 @@ done
 
 # Check if only one flag is set
 if [[ $client_id == true && $secret == false && $password == false ]]; then
-   CLIENTID=$(az keyvault secret show --vault-name bitwarden-creds-keyvault --name bw-alex-gonzalez-api-key-client-id --query value -o tsv)
+   CLIENTID=$(az keyvault secret show --vault-name bw-vault-name --name api-client-id-item-here --query value -o tsv)
    echo $CLIENTID
 
 elif [[ $client_id == false && $secret == true && $password == false ]]; then
-   SECRET=$(az keyvault secret show --vault-name bitwarden-creds-keyvault --name bw-alex-gonzalez-api-key-secret --query value -o tsv)
+   SECRET=$(az keyvault secret show --vault-name bw-vault-name --name api-secret-item-here --query value -o tsv)
    echo $CLIENTSECRET
 
 elif [[ $client_id == false && $secret == false && $password == true ]]; then
-   PASSWORD=$(az keyvault secret show --vault-name bitwarden-creds-keyvault --name bw-alex-gonzalez-pw --query value -o tsv)
+   PASSWORD=$(az keyvault secret show --vault-name bw-vault-name --name password-item-here --query value -o tsv)
    echo $PASSWORD
 else
   echo "Error: Please specify only one flag at a time (-c, -s, or -p)"
